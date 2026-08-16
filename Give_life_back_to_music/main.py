eee777796c24df738f8859ee09d51d0acfb4a97c
6urevestnik <dangrace6q@gmail.com>
2026-08-16 22:08:44 +0700

    Gave_life_back_to_music

---
+++ Give_life_back_to_music/main.py
@@ -0,0 +1,34 @@
+print('|---------------------------------------|')
+print('|* Daft_Punk - Give_Life_Back_to_Music *|')
+print('|---------------------------------------|')
+
+for music in range (4):
+    music = ('|       Let the music in tonight        |')
+    music_2 = ('|        Just turn on the music         |')
+    music_3 = ('|      Let the music of your life       |')
+    music_4 = ('|        Give life back to music        |')
+
+    print(music)
+    print(music_2)
+    print(music_3)
+    print(music_4)
+    print('|---------------------------------------|')
+
+for music in range (2):
+    music_4 = ('|        Give life back to music        |')
+    print(music_4)
+print('|---------------------------------------|')
+
+for music in range (4):
+    music = ('|       Let the music in tonight        |')
+    music_2 = ('|        Just turn on the music         |')
+    music_3 = ('|      Let the music of your life       |')
+    music_4 = ('|        Give life back to music        |')
+
+    print(music)
+    print(music_2)
+    print(music_3)
+    print(music_4)
+    print('|---------------------------------------|')
+
+print('in_progress...')
